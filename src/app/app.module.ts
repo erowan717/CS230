e1949d9748b870ac89b55cae7bca5d322e4ac406
erowan717 <ejr0010@mix.wvu.edu>
2021-09-09 18:49:49 -0400

    Enter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { TopHeadingComponent } from './header/top-heading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    TopHeadingComponent,
   ],
   imports: [
     BrowserModule
